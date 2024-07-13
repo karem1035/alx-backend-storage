6ec0ce4f1195736448a320ffbe5467afe01a5f1f
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-07-13 16:33:29 +0300

    task 6 another try

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,33 +1,34 @@
--- 0x00-MySQL_Advanced/6-bonus.sql
---
--- This file contains a stored procedure that adds a bonus correction to a project.
--- If the project doesn't exist, it creates it.
---
--- Parameters:
---   user_id (IN) - id of the user to add the correction to
---   project_name (IN) - name of the project to add the correction to
---   score (IN) - score of the correction
 DELIMITER //
 
-CREATE PROCEDURE AddBonus(
-	IN user_id  INT,
+-- AddBonus procedure
+-- Add a bonus correction to a user for a project
+-- Parameters:
+--  - user_id: the user's id
+--  - project_name: the name of the project
+--  - score: the score of the correction
+CREATE PROCEDURE AddBonus (
+	IN user_id INT,
 	IN project_name VARCHAR(255),
 	IN score INT
 )
 BEGIN
-	DECLARE project_id INT;
-	SELECT id INTO project_id
+	DECLARE project_id INT;  -- Declare a variable to store the project ID
+
+	-- Try to get the project ID from the projects table
+	SELECT id
+	INTO project_id
 	FROM projects
 	WHERE name = project_name;
 
-	-- check if project exists
+	-- If the project does not exist, insert it into the projects table
 	IF project_id IS NULL THEN
 		INSERT INTO projects (name) VALUES (project_name);
+		-- Retrieve the ID of the newly inserted project
 		SET project_id = LAST_INSERT_ID();
 	END IF;
-	
-	-- Add correction
+
+	-- Insert the bonus correction into the corrections table
 	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END //
+END//
 
 DELIMITER ;
