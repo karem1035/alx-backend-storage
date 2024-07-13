7e969fab6b1d50f24546014d66e4b5ac077307bf
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-07-13 17:44:12 +0300

    first name index

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,2 @@
--- Creates an index idx_name_first on table names and first letter of name
--- Only first letter of name must be indexed
-
-CREATE INDEX idx_name_first
-ON names(name(1));
+-- Create a Index with the first name
+CREATE INDEX idx_name_first ON names(name(1))
