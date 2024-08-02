71fc733698005eca8c80a5c1885f37d3c707fa3f
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-08-02 16:51:44 +0300

    task 10 updated

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,13 +7,5 @@ Module for updating topics in school documents in a MongoDB collection.
 def update_topics(mongo_collection, name, topics):
     """
     Update all school documents with name = name in mongo_collection with topics = topics.
-
-    Args:
-        mongo_collection(MongoClient): The MongoDB collection to update.
-        name(str): The name of the school.
-        topics(list): The new topics for the school.
-
-    Returns:
-        None.
     """
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
