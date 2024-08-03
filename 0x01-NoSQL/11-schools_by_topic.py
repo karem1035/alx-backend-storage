1241e26e3e1632caed07f5b3bea53506bc78cc6f
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-08-03 08:44:52 +0300

    task 11 done

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """return the matcing topic"""
-    return list(mongo_collection.coll.find({"topics": topic}))
+    documents = mongo_collection.find({"topics": topic})
+    return list(documents)
